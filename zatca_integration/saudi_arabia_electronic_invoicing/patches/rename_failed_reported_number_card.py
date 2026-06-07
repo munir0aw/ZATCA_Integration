3a22d51020998f3a26e37bf6e90d99507b2f014a
Munir Aw <munir0aw0@gmail.com>
2026-06-07 05:45:49 +0300

    feat: Refactor number card renaming logic to improve error handling and ensure proper deletion of existing records

--- zatca_integration/saudi_arabia_electronic_invoicing/patches/rename_failed_reported_number_card.py
+++ zatca_integration/saudi_arabia_electronic_invoicing/patches/rename_failed_reported_number_card.py
@@ -2,13 +2,14 @@ import frappe
 
 
 def execute():
-	if frappe.db.exists("Number Card", "NOT REPORTED INVOICES") and not frappe.db.exists(
-		"Number Card", "FAILED REPORTED INVOICES"
-	):
-		frappe.rename_doc(
-			"Number Card",
-			"NOT REPORTED INVOICES",
-			"FAILED REPORTED INVOICES",
-			force=True,
-			merge=True,
-		)
+	old_name = "NOT REPORTED INVOICES"
+	new_name = "FAILED REPORTED INVOICES"
+
+	if not frappe.db.exists("Number Card", old_name):
+		return
+
+	if frappe.db.exists("Number Card", new_name):
+		frappe.delete_doc("Number Card", old_name, force=True)
+		return
+
+	frappe.rename_doc("Number Card", old_name, new_name, force=True)
